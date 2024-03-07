d58ddadcd58b117acdd5dbe6a1a6b618911f34f7
Patryk <patryk.figiel8@gmail.com>
2024-03-08 00:52:36 +0100

    Initial commit

---
+++ src/main/java/com/plantie/app/plantie/PlantieApplication.java
@@ -0,0 +1,13 @@
+package com.plantie.app.plantie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantie/app/plantie/PlantieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantie.app.plantie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
