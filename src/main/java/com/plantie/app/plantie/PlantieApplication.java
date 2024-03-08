9a8fc0774237942e53fa44163542a6ffffb267f7
Patryk <patryk.figiel8@gmail.com>
2024-03-08 15:13:28 +0100

    Updated spring security

--- src/main/java/com/plantie/app/plantie/PlantieApplication.java
+++ src/main/java/com/plantie/app/plantie/PlantieApplication.java
@@ -2,15 +2,9 @@ package com.plantie.app.plantie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class PlantieApplication {
-
-    @GetMapping("/index")
-    public String message(){
-        return "Hi!";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(PlantieApplication.class, args);
