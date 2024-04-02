3340d2db2896924d68f6b2dc658816f795e4be3e
Patryk <patryk.figiel8@gmail.com>
2024-04-02 16:39:23 +0200

    change spring version

--- src/main/java/com/plantie/app/plantie/config/SecurityConfig.java
+++ src/main/java/com/plantie/app/plantie/config/SecurityConfig.java
@@ -1,20 +1,23 @@
 package com.plantie.app.plantie.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .anyRequest().permitAll()
-                .and()
-                .csrf().disable();
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
     }
+
 }
--- src/main/java/com/plantie/app/plantie/controller/NotificationController.java
+++ src/main/java/com/plantie/app/plantie/controller/NotificationController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
+
 
 @RestController
 public class NotificationController {
@@ -17,8 +17,8 @@ public class NotificationController {
         this.notificationService = notificationService;
     }
 
-    @GetMapping("/notifications")
-    public Flux<ServerSentEvent<String>> getNotifications() {
-        return notificationService.getNotifications();
-    }
+//    @GetMapping("/notifications")
+//    public Flux<ServerSentEvent<String>> getNotifications() {
+//        return notificationService.getNotifications();
+//    }
 }
